b4840626c3f3658fe86677e7d54a0cb6879a7b90
TitouWaley <235715725+TitouWaley@users.noreply.github.com>
2025-10-09 17:30:59 +0200

    Je suis mort

--- TPV2/tests/test_file_io.py
+++ TPV2/tests/test_file_io.py
@@ -1,16 +1,16 @@
-import os
-from src.file_manager import BibliothequeAvecFichier
 from src.models import Livre
+from src.file_manager import BibliothequeAvecFichier
 
-
-def test_sauvegarde_et_chargement(a):
-    path = a / "catalogue.json"
+def test_sauvegarde_et_chargement(tmp_path):
     b = BibliothequeAvecFichier("Test")
     b.ajouter_livre(Livre("Titre", "Auteur", "123"))
-    b.sauvegarder_json(path)
-    assert path.exists()
 
-    b2 = BibliothequeAvecFichier.charger_json(path)
-    assert b2.nom == "Test"
+    b.sauvegarder_json()
+
+    b2 = BibliothequeAvecFichier.charger_json()
+
     assert len(b2.livres) == 1
     assert b2.livres[0].titre == "Titre"
+    assert b2.livres[0].auteur == "Auteur"
+
+#J'ai trouvé en lisant les erreurs une pas une.
