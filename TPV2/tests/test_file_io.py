acf6787f70483923f2e99d4db5aeaafa274d29c5
TitouWaley <235715725+TitouWaley@users.noreply.github.com>
2025-10-09 17:16:52 +0200

    Je suis mort

--- TPV2/tests/test_file_io.py
+++ TPV2/tests/test_file_io.py
@@ -3,8 +3,8 @@ from src.file_manager import BibliothequeAvecFichier
 from src.models import Livre
 
 
-def test_sauvegarde_et_chargement(tmp_path):
-    path = tmp_path / "catalogue.json"
+def test_sauvegarde_et_chargement(a):
+    path = a / "catalogue.json"
     b = BibliothequeAvecFichier("Test")
     b.ajouter_livre(Livre("Titre", "Auteur", "123"))
     b.sauvegarder_json(path)
